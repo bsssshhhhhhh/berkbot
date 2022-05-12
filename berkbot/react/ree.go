e65629c3fda4a0bf70ee2802967d98aace8ec99a
Brian Simon <bsssshhhhhhh@gmail.com>
2022-05-12 19:24:22 -0400

    ree

--- berkbot/react/ree.go
+++ berkbot/react/ree.go
@@ -8,7 +8,7 @@ import (
 )
 
 func ReeReaction(session *discordgo.Session, message *discordgo.MessageCreate) (*discordgo.Emoji, *string) {
-	match, _ := regexp.MatchString("\bre{2,}", message.Content)
+	match, _ := regexp.MatchString("\\bre{2,}", message.Content)
 
 	if match {
 		emoji := berkbot_utils.FindGuildEmoji(session, message.GuildID, "ree")
